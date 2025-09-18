c45ecbf76bbe2680417a858c07785563ef68389a
AlR0507 <160520283+AlR0507@users.noreply.github.com>
2025-09-17 21:54:05 -0700

    Add files via upload

---
+++ SeriesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NumericSeries.Models
+{
+    public class SeriesViewModel
+    {
+        public string Series { get; set; } = string.Empty;
+        public int N { get; set; }
+        public List<int> Result { get; set; }
+        public string Message { get; set; } 
+    }
+}
