0b8c7e248bc1ba3ef51acf7868a4a07c7b64b9c5
AlR0507 <160520283+AlR0507@users.noreply.github.com>
2025-10-07 19:24:20 -0700

    Add files via upload

---
+++ Order Notifications/NotificationService.cs
@@ -0,0 +1,60 @@
+﻿using OrderNotifications.Models;
+using System;
+using System.Runtime.InteropServices;
+
+namespace OrderNotifications
+{
+    public class NotificationService : INotificationService
+    {
+        public void NotifyOrderStatus(Order order)
+        {
+            foreach (NotificationChannel channel in Enum.GetValues<NotificationChannel>())
+            {
+                if (ShouldNotify(order.Customer, channel))
+                {
+                    SendNotification(order, channel);
+                }
+            }
+        }
+
+        private bool ShouldNotify(Customer customer, NotificationChannel channel)
+        {
+            if(customer.Preferences.TryGetValue(channel, out var preference))
+            {
+                return preference == NotificationChannelPreference.Enabled;
+            }
+
+            if (customer.CountryCode == "AK")
+            {
+                
+                    return false;
+            }
+
+            return channel switch
+            {
+                NotificationChannel.Email => true,
+                NotificationChannel.SMS => customer.CountryCode == "MX" ? false : true,
+                NotificationChannel.WhatsApp => customer.CountryCode == "MX" ? true : false,
+            };
+        }
+
+        private void SendNotification(Order order, NotificationChannel channel)
+        {
+            var message = $"Order {order.Id} is now {order.Status}";
+            var contact = order.Customer.ContactInfo;
+
+            switch (channel)
+            {
+                case NotificationChannel.Email:
+                    Console.WriteLine($"Sending Email to {contact.Email}: {message}");
+                    break;
+                case NotificationChannel.SMS:
+                    Console.WriteLine($"Sending SMS to {contact.PhoneNumber}: {message}");
+                    break;
+                case NotificationChannel.WhatsApp:
+                    Console.WriteLine($"Sending WhatsApp message to {contact.PhoneNumber}: {message}");
+                    break;
+            }
+        }
+    }
+}
