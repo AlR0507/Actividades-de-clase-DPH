f9c3fe6d5558fa0d59d837b363b113412076a1f8
AlR0507 <160520283+AlR0507@users.noreply.github.com>
2025-10-30 23:17:31 -0700

    Add files via upload

---
+++ 8. Protegiendo-Un-Sitio-Web/Filters/RequireAuthAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Protegiendo_un_sitio_web.Middleware;
+
+
+namespace Protegiendo_un_sitio_web.Filters
+{
+    public class RequireAuthAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Items["UserId"] == null)
+            {
+                context.Result = new RedirectToActionResult("Login", "Account", new { returnUrl = context.HttpContext.Request.Path });
+            }
+        }
+    }
+}
