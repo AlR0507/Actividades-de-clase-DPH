5c063c43662fb9d49c06dfd9de68dc83596146a9
AlR0507 <160520283+AlR0507@users.noreply.github.com>
2025-10-30 23:21:26 -0700

    Add files via upload

---
+++ 8. Protegiendo-Un-Sitio-Web/Views/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Protegiendo_un_sitio_web.Views.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
