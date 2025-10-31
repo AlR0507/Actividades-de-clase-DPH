70022b627296ddd05c6e33aa2de940cdb4100da9
AlR0507 <160520283+AlR0507@users.noreply.github.com>
2025-10-30 23:21:55 -0700

    Add files via upload

---
+++ 8. Protegiendo-Un-Sitio-Web/Views/Dashboard/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Protegiendo_un_sitio_web.Views.Dashboard
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
