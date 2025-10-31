77da3563568dd6439ea1f20838113a8f31af076c
AlR0507 <160520283+AlR0507@users.noreply.github.com>
2025-10-30 23:17:08 -0700

    Add files via upload

---
+++ 8. Protegiendo-Un-Sitio-Web/Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Protegiendo_un_sitio_web.Models;
+
+
+namespace Protegiendo_un_sitio_web.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<UserSession> Sessions => Set<UserSession>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(e =>
+            {
+                e.HasIndex(u => u.Username).IsUnique();
+                e.HasIndex(u => u.Email).IsUnique();
+            });
+
+
+            modelBuilder.Entity<UserSession>(e =>
+            {
+                e.HasIndex(s => s.SessionId).IsUnique();
+            });
+        }
+    }
+}
