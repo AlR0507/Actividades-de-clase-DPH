f38043ce0b9b597b904381789fd61d5dd244fbdf
AlR0507 <160520283+AlR0507@users.noreply.github.com>
2025-11-06 23:14:50 -0800

    Add files via upload

---
+++ Protegiendo una API/Program.cs
@@ -0,0 +1,69 @@
+﻿using Comprehension.Data;
+using Microsoft.EntityFrameworkCore;
+using Comprehension.Auth;
+
+namespace Comprehension
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Controllers
+            builder.Services.AddControllers();
+
+            // Swagger con seguridad
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Description = "Ingresa tu token en el formato: Bearer {token}",
+                    Name = "Authorization",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                 Id = "Bearer"
+                            }
+                        },
+
+                        new string[] {}
+                    }
+                });
+            });
+
+
+            builder.Services.AddDbContext<ComprehensionContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("ComprehensionContext") ?? throw new InvalidOperationException("Connection string 'ComprehensionContext' not found.")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
